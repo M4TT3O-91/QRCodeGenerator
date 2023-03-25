2911a64165d1e059bdccd13c8767733956e669b6
M4TT3O-91 <teo_exc@hotmail.it>
2023-03-25 14:31:39 +0100

    Aggiunto dipendenza QR coder Modifica UI

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,16 +1,16 @@
-﻿namespace QRCodeGenerator
+﻿namespace QRCoderDemo
 {
     partial class Form1
     {
         /// <summary>
-        /// Variabile di progettazione necessaria.
+        /// Erforderliche Designervariable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Pulire le risorse in uso.
+        /// Verwendete Ressourcen bereinigen.
         /// </summary>
-        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,21 +20,272 @@
             base.Dispose(disposing);
         }
 
-        #region Codice generato da Progettazione Windows Form
+        #region Vom Windows Form-Designer generierter Code
 
         /// <summary>
-        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
-        /// il contenuto del metodo con l'editor di codice.
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.textBoxQRCode = new System.Windows.Forms.TextBox();
+            this.pictureBoxQRCode = new System.Windows.Forms.PictureBox();
+            this.comboBoxECC = new System.Windows.Forms.ComboBox();
+            this.labelECC = new System.Windows.Forms.Label();
+            this.labelIcon = new System.Windows.Forms.Label();
+            this.iconPath = new System.Windows.Forms.TextBox();
+            this.selectIconBtn = new System.Windows.Forms.Button();
+            this.labelIconsize = new System.Windows.Forms.Label();
+            this.iconSize = new System.Windows.Forms.NumericUpDown();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.colorDialogPrimaryColor = new System.Windows.Forms.ColorDialog();
+            this.labelPreviewPrimaryColor = new System.Windows.Forms.Label();
+            this.panelPreviewPrimaryColor = new System.Windows.Forms.Panel();
+            this.labelPreviewBackgroundColor = new System.Windows.Forms.Label();
+            this.panelPreviewBackgroundColor = new System.Windows.Forms.Panel();
+            this.colorDialogBackgroundColor = new System.Windows.Forms.ColorDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxQRCode)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconSize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonGenerate.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonGenerate.Location = new System.Drawing.Point(587, 28);
+            this.buttonGenerate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(105, 52);
+            this.buttonGenerate.TabIndex = 0;
+            this.buttonGenerate.Text = "Generate QR";
+            this.buttonGenerate.UseVisualStyleBackColor = true;
+            this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerate_Click);
+            // 
+            // textBoxQRCode
+            // 
+            this.textBoxQRCode.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxQRCode.Location = new System.Drawing.Point(53, 28);
+            this.textBoxQRCode.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxQRCode.Name = "textBoxQRCode";
+            this.textBoxQRCode.Size = new System.Drawing.Size(524, 22);
+            this.textBoxQRCode.TabIndex = 1;
+            this.textBoxQRCode.Text = "1234567890";
+            this.textBoxQRCode.TextChanged += new System.EventHandler(this.textBoxQRCode_TextChanged);
+            // 
+            // pictureBoxQRCode
+            // 
+            this.pictureBoxQRCode.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxQRCode.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBoxQRCode.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxQRCode.Location = new System.Drawing.Point(53, 130);
+            this.pictureBoxQRCode.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBoxQRCode.Name = "pictureBoxQRCode";
+            this.pictureBoxQRCode.Size = new System.Drawing.Size(615, 514);
+            this.pictureBoxQRCode.TabIndex = 2;
+            this.pictureBoxQRCode.TabStop = false;
+            // 
+            // comboBoxECC
+            // 
+            this.comboBoxECC.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxECC.FormattingEnabled = true;
+            this.comboBoxECC.Items.AddRange(new object[] {
+            "L",
+            "M",
+            "Q",
+            "H"});
+            this.comboBoxECC.Location = new System.Drawing.Point(129, 60);
+            this.comboBoxECC.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBoxECC.Name = "comboBoxECC";
+            this.comboBoxECC.Size = new System.Drawing.Size(165, 24);
+            this.comboBoxECC.TabIndex = 3;
+            this.comboBoxECC.SelectedIndexChanged += new System.EventHandler(this.comboBoxECC_SelectedIndexChanged);
+            // 
+            // labelECC
+            // 
+            this.labelECC.AutoSize = true;
+            this.labelECC.Location = new System.Drawing.Point(49, 64);
+            this.labelECC.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelECC.Name = "labelECC";
+            this.labelECC.Size = new System.Drawing.Size(71, 16);
+            this.labelECC.TabIndex = 4;
+            this.labelECC.Text = "ECC-Level";
+            // 
+            // labelIcon
+            // 
+            this.labelIcon.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelIcon.AutoSize = true;
+            this.labelIcon.Location = new System.Drawing.Point(50, 690);
+            this.labelIcon.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelIcon.Name = "labelIcon";
+            this.labelIcon.Size = new System.Drawing.Size(35, 16);
+            this.labelIcon.TabIndex = 5;
+            this.labelIcon.Text = "Icon:";
+            // 
+            // iconPath
+            // 
+            this.iconPath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.iconPath.Location = new System.Drawing.Point(99, 686);
+            this.iconPath.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.iconPath.Name = "iconPath";
+            this.iconPath.Size = new System.Drawing.Size(211, 22);
+            this.iconPath.TabIndex = 6;
+            // 
+            // selectIconBtn
+            // 
+            this.selectIconBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.selectIconBtn.Location = new System.Drawing.Point(318, 682);
+            this.selectIconBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.selectIconBtn.Name = "selectIconBtn";
+            this.selectIconBtn.Size = new System.Drawing.Size(81, 31);
+            this.selectIconBtn.TabIndex = 7;
+            this.selectIconBtn.Text = "Select";
+            this.selectIconBtn.UseVisualStyleBackColor = true;
+            this.selectIconBtn.Click += new System.EventHandler(this.selectIconBtn_Click);
+            // 
+            // labelIconsize
+            // 
+            this.labelIconsize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelIconsize.AutoSize = true;
+            this.labelIconsize.Location = new System.Drawing.Point(49, 661);
+            this.labelIconsize.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelIconsize.Name = "labelIconsize";
+            this.labelIconsize.Size = new System.Drawing.Size(87, 16);
+            this.labelIconsize.TabIndex = 8;
+            this.labelIconsize.Text = "Icon size (px):";
+            // 
+            // iconSize
+            // 
+            this.iconSize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.iconSize.Location = new System.Drawing.Point(143, 657);
+            this.iconSize.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.iconSize.Name = "iconSize";
+            this.iconSize.Size = new System.Drawing.Size(167, 22);
+            this.iconSize.TabIndex = 9;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSave.Location = new System.Drawing.Point(525, 654);
+            this.buttonSave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(143, 52);
+            this.buttonSave.TabIndex = 10;
+            this.buttonSave.Text = "Save QR";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // labelPreviewPrimaryColor
+            // 
+            this.labelPreviewPrimaryColor.AutoSize = true;
+            this.labelPreviewPrimaryColor.Location = new System.Drawing.Point(49, 101);
+            this.labelPreviewPrimaryColor.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelPreviewPrimaryColor.Name = "labelPreviewPrimaryColor";
+            this.labelPreviewPrimaryColor.Size = new System.Drawing.Size(86, 16);
+            this.labelPreviewPrimaryColor.TabIndex = 12;
+            this.labelPreviewPrimaryColor.Text = "Primary color";
+            // 
+            // panelPreviewPrimaryColor
+            // 
+            this.panelPreviewPrimaryColor.BackColor = System.Drawing.Color.Black;
+            this.panelPreviewPrimaryColor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelPreviewPrimaryColor.Location = new System.Drawing.Point(143, 94);
+            this.panelPreviewPrimaryColor.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panelPreviewPrimaryColor.Name = "panelPreviewPrimaryColor";
+            this.panelPreviewPrimaryColor.Size = new System.Drawing.Size(63, 29);
+            this.panelPreviewPrimaryColor.TabIndex = 13;
+            this.panelPreviewPrimaryColor.Click += new System.EventHandler(this.panelPreviewPrimaryColor_Click);
+            // 
+            // labelPreviewBackgroundColor
+            // 
+            this.labelPreviewBackgroundColor.AutoSize = true;
+            this.labelPreviewBackgroundColor.Location = new System.Drawing.Point(484, 101);
+            this.labelPreviewBackgroundColor.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelPreviewBackgroundColor.Name = "labelPreviewBackgroundColor";
+            this.labelPreviewBackgroundColor.Size = new System.Drawing.Size(113, 16);
+            this.labelPreviewBackgroundColor.TabIndex = 14;
+            this.labelPreviewBackgroundColor.Text = "Background color";
+            // 
+            // panelPreviewBackgroundColor
+            // 
+            this.panelPreviewBackgroundColor.BackColor = System.Drawing.Color.White;
+            this.panelPreviewBackgroundColor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelPreviewBackgroundColor.Location = new System.Drawing.Point(605, 94);
+            this.panelPreviewBackgroundColor.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panelPreviewBackgroundColor.Name = "panelPreviewBackgroundColor";
+            this.panelPreviewBackgroundColor.Size = new System.Drawing.Size(63, 29);
+            this.panelPreviewBackgroundColor.TabIndex = 15;
+            this.panelPreviewBackgroundColor.Click += new System.EventHandler(this.panelPreviewBackgroundColor_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(50, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 16);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "TYPE TEXT TO ENCODING";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(724, 736);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panelPreviewBackgroundColor);
+            this.Controls.Add(this.labelPreviewBackgroundColor);
+            this.Controls.Add(this.panelPreviewPrimaryColor);
+            this.Controls.Add(this.labelPreviewPrimaryColor);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.iconSize);
+            this.Controls.Add(this.labelIconsize);
+            this.Controls.Add(this.selectIconBtn);
+            this.Controls.Add(this.iconPath);
+            this.Controls.Add(this.labelIcon);
+            this.Controls.Add(this.labelECC);
+            this.Controls.Add(this.comboBoxECC);
+            this.Controls.Add(this.pictureBoxQRCode);
+            this.Controls.Add(this.textBoxQRCode);
+            this.Controls.Add(this.buttonGenerate);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MinimumSize = new System.Drawing.Size(739, 730);
+            this.Name = "Form1";
+            this.Text = "QRCoder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxQRCode)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconSize)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonGenerate;
+        private System.Windows.Forms.TextBox textBoxQRCode;
+        private System.Windows.Forms.PictureBox pictureBoxQRCode;
+        private System.Windows.Forms.ComboBox comboBoxECC;
+        private System.Windows.Forms.Label labelECC;
+        private System.Windows.Forms.Label labelIcon;
+        private System.Windows.Forms.TextBox iconPath;
+        private System.Windows.Forms.Button selectIconBtn;
+        private System.Windows.Forms.Label labelIconsize;
+        private System.Windows.Forms.NumericUpDown iconSize;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.ColorDialog colorDialogPrimaryColor;
+        private System.Windows.Forms.Label labelPreviewPrimaryColor;
+        private System.Windows.Forms.Panel panelPreviewPrimaryColor;
+        private System.Windows.Forms.Label labelPreviewBackgroundColor;
+        private System.Windows.Forms.Panel panelPreviewBackgroundColor;
+        private System.Windows.Forms.ColorDialog colorDialogBackgroundColor;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Program.cs
+++ Program.cs
@@ -1,21 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace QRCodeGenerator
+namespace QRCoderDemo
 {
-    internal static class Program
+    static class Program
     {
         /// <summary>
-        /// Punto di ingresso principale dell'applicazione.
+        /// Der Haupteinstiegspunkt für die Anwendung.
         /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
             Application.Run(new Form1());
         }
     }
